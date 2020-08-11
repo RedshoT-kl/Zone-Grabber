0bfc44431cde4899ac4cf1e6b733d3563942339e
RedshoT-kl <66035993+RedshoT-kl@users.noreply.github.com>
2020-08-11 03:13:41 -0700

    Add files via upload

---
+++ config/roote.php
@@ -0,0 +1,10 @@
+<?php
+#-----------------------------🌿 RedshoT 🌿-------------------------------#
+//files
+
+include 'Files/Banner.php';
+include 'Files/curl.php';
+include 'Files/config.php';
+
+#-----------------------------🌿 RedshoT 🌿-------------------------------#
+?>
